2a40bb39ca57e9c4affba42e94fe14e5ea452e59
cshm <cshm225@gmail.com>
2022-07-05 06:53:24 +0900

    a

--- macro/main.py
+++ macro/main.py
@@ -8,107 +8,133 @@ def main():
     aapo = am.AapoManager(adbpath)
 
     def event():
-        imgs = ["event1.PNG",
-                "event2.PNG",
-                "event3.PNG",
-                "event4.PNG",
-                "event5.PNG",
-                "event6.PNG",
-                "event7.PNG",
-                "event8.PNG"]
+        imgs = [
+            "event1.PNG",
+            "event2.PNG",
+            "event3.PNG",
+            "event4.PNG",
+            "event5.PNG",
+            "event6.PNG",
+            "event7.PNG",
+            "event8.PNG"
+        ]
 
-        imgpath = "event"
-        count = 1
         aapo.screencap()
         aapo.touchImg("./images/work.PNG")
+        aapo.sleep(1)
 
         for imgpath in imgs:
             while True:
                 aapo.screencap()
-                if imgpath==imgs[2]:
+                if imgpath == imgs[2]:
                     if aapo.chkImg("./images/event/"+imgpath):
-                        aapo.touchPos(1710,930)
+                        aapo.touchPos(1710, 930)
                         break
-                if imgpath==imgs[3]:
+                elif imgpath == imgs[3]:
                     result, x, y = aapo.chkImg2("./images/event/"+imgpath)
                     if result:
-                        aapo.longTouchPos(x, y, 11000)
+                        aapo.longTouchPos(x, y, 10000)
                         break
-                if aapo.chkImg("./images/event/"+imgpath):
-                    aapo.touchImg("./images/event/"+imgpath)
-                    break
-
-
-
-        while True:
-            count += 1
-            aapo.sleep(3)
-            aapo.screencap()
-            aapo.sleep(3)
-            if count == 3:
-                aapo.touchPos(1710, 930)
-            else:
-                if count == 4:
-                    result, x, y = aapo.chkImg2(
-                        "./images/event/"+imgpath+str(count)+".PNG")
-                    if result:
-                        aapo.longTouchPos(x, y, 11000)
+                elif imgpath == imgs[7]:
+                    if aapo.chkImg("./images/event/"+imgpath):
+                        aapo.sleep(1)
+                        aapo.touchPos(1615.5, 145)
+                        break
                 else:
-                    aapo.touchImg("./images/event/"+imgpath+str(count)+".PNG")
-                if count == 8:
-                    break
-        aapo.sleep(3)
+                    if aapo.chkImg("./images/event/"+imgpath):
+                        aapo.sleep(1)
+                        aapo.touchImg("./images/event/"+imgpath)
+                        break
+        aapo.screencap()
         aapo.touchImg("./images/home.PNG")
         aapo.sleep(2)
         aapo.touchImg("./images/home.PNG")
 
     def cafe():
-        imgpath = "cafe"
-        count = 0
+        cafepath = "./images/cafe/"
+        imgs = [
+            "cafe1.PNG",
+            "cafe2.PNG",
+            "cafe3.PNG",
+            "cafe4.PNG",
+            "cafe5.PNG",
+            "cafe6.PNG",
+        ]
+
+        # 処理開始
+        aapo.screencap()
+        aapo.touchImg("./images/cafe.PNG")
         while True:
-            count += 1
             aapo.screencap()
-            aapo.sleep(4)
-            aapo.touchImg("./images/cafe/"+imgpath+str(count)+".PNG")
-            aapo.sleep(4)
-            if count == 6:
-                break
+            aapo.chkImg(cafepath+"cafein.PNG")
+            break
+        aapo.sleep(3)
+
+        # cafeに入ってからの処理
+        for imgpath in imgs:
+            while True:
+                aapo.screencap()
+                if imgpath == imgs[0]:
+                    aapo.sleep(5)
+                    if aapo.chkImg(cafepath+imgpath):
+                        aapo.touchPos(600, 600)
+                        break
+                    else:
+                        break
+                else:
+                    if aapo.chkImg(cafepath+imgpath):
+                        aapo.touchImg(cafepath+imgpath)
+                        break
 
     def battle():
+        battlepath = "./images/battle/"
+        imgs = [
+            "battle1.PNG",
+            "battle2.PNG",
+            "battle3.PNG",
+        ]
+
+        # 処理開始
         aapo.screencap()
         aapo.touchImg("./images/work.PNG")
-        imgpath = "battle"
-        count = 0
-        while True:
-            count += 1
-            aapo.sleep(4)
-            aapo.screencap()
-            aapo.sleep(4)
-            if count == 3:
-                aapo.touchPos(539, 698)
-                aapo.sleep(3)
-                aapo.touchPos(1800, 1000)
-                break
-            else:
-                aapo.touchImg("./images/battle/"+imgpath+str(count)+".PNG")
-            if count == 2:
-                aapo.sleep(5)
-                aapo.touchPos(1800, 1000)
+        for imgpath in imgs:
+            while True:
+                aapo.screencap()
+                if aapo.chkImg(battlepath+imgpath):
+                    if imgpath == imgs[1]:
+                        aapo.touchPos(1800, 1000)
+                        break
+                    elif imgpath == imgs[2]:
+                        aapo.touchPos(539, 598)
+                        aapo.sleep(3)
+                        aapo.touchPos(1800, 1000)
+                        break
+                    else:
+                        aapo.touchImg(battlepath+imgpath)
+                        break
+        aapo.screencap()
         aapo.touchImg("./images/home.PNG")
 
     def shop():
+        shoppath = "./images/shop/"
+        imgs = [
+            "shop1.PNG",
+            "shop2.PNG",
+            "shop3.PNG",
+            "shop4.PNG",
+            "shop5.PNG",
+        ]
         aapo.screencap()
-        aapo.touchImg("./images/shop/shop.PNG")
-        imgpath = "shop"
-        count = 0
-        while True:
-            count += 1
-            aapo.sleep(3)
-            aapo.screencap()
-            aapo.sleep(3)
-            aapo.touchImg("./images/shop/"+imgpath+str(count)+".PNG")
-            if count == 5:
-                break
+        aapo.touchImg("./images/shop.PNG")
+
+        for imgpath in imgs:
+            while True:
+                aapo.screencap()
+                if aapo.chkImg(shoppath+imgpath):
+                    aapo.touchImg(shoppath+imgpath)
+                    break
+
+        aapo.screencap()
         aapo.touchImg("./images/home.PNG")
 
     cafe()
