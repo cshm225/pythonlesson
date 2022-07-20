023d9be1a6710982170f09200a51fea492df9939
cshm <cshm225@gmail.com>
2022-07-20 20:37:55 +0900

    a

--- tkinter/spaceship/star.py
+++ tkinter/spaceship/star.py
@@ -1,5 +1,9 @@
+from email.mime import image
+from pickle import TRUE
 import tkinter as tk
 import random
+
+from pygame import init
 
 fnt1 = ("Times New Roman", 24)
 fnt2 = ("Times New Roman", 50)
@@ -18,7 +22,7 @@ koff = False
 
 
 def key_down(e):
-    global key, koff
+    global koff, key
     key = e.keysym
     koff = False
 
@@ -36,36 +40,36 @@ def main():
     canvas.create_image(240, bg_pos-320, image=img_bg, tag="SCREEN")
     canvas.create_image(240, bg_pos+320, image=img_bg, tag="SCREEN")
     if index == 0:
-        canvas.create_text(240, 240, text="METEOR",
+        canvas.create_text(240, 240, text="METOR",
                            fill="gold", font=fnt2, tag="SCREEN")
-        canvas.create_text(
-            240, 480, text="Press[SPACE] Key", fill="lime", font=fnt1, tag="SCREEN")
+        canvas.create_text(240, 480, text="pressspace",
+                           fill="lime", font=fnt1, tag="SCREEN")
         if key == "space":
             score = 0
             px = 240
             init_enemy()
             index = 1
-        if index == 1:
-            score = score + 1
-            move_player()
-            move_enemy()
-        if index == 2:
-            move_enemy()
-            canvas.create_text(240, timer*4, text="GAME OVER",
-                               fill="red", font=fnt2, tag="SCREEN")
-            if timer == 60:
-                index = 0
-                timer = 0
-        canvas.create_text(240, 30, text="SCORE "+str(score),
-                           fill="white", font=fnt1, tag="SCREEN")
-        if koff == True:
-            key = ""
-            koff = False
-        root.after(50, main)
+    if index == 1:
+        score = score + 1
+        move_player()
+        move_enemy()
+    if index == 2:
+        move_enemy()
+        canvas.create_text(240, timer*4, text="GAMEOVER",
+                           fill="red", font=fnt2, tag="SCREEN")
+        if timer == 60:
+            index = 0
+            timer = 0
+    canvas.create_text(240, 30, text="SCORE"+str(score),
+                       fill="white", font=fnt1, tag="SCREEN")
+    if koff == True:
+        key = ""
+        koff = False
+    root.after(50, main)
 
 
 def hit_check(x1, y1, x2, y2):
-    if((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) < 36*36):
+    if((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2) < 36*36):
         return True
     return False
 
@@ -79,7 +83,7 @@ def init_enemy():
 def move_enemy():
     global index, timer
     for i in range(METEO_MAX):
-        my[i] = my[i] + 6+i/5
+        my[i] = my[i]+6+i/5
         if my[i] > 660:
             mx[i] = random.randint(0, 480)
             my[i] = random.randint(-640, 0)
@@ -92,14 +96,14 @@ def move_enemy():
 def move_player():
     global px
     if key == "Left" and px > 30:
-        px = px - 10
+        px = px-10
     if key == "Right" and px < 450:
-        px = px + 10
+        px = px+10
     canvas.create_image(px, py, image=img_player[timer % 2], tag="SCREEN")
 
 
 root = tk.Tk()
-root.title("Mini Game")
+root.title("a")
 root.bind("<KeyPress>", key_down)
 root.bind("<KeyRelease>", key_up)
 canvas = tk.Canvas(width=480, height=640)
@@ -108,6 +112,7 @@ img_player = [
     tk.PhotoImage(file="starship0.png"),
     tk.PhotoImage(file="starship1.png")
 ]
+
 img_enemy = tk.PhotoImage(file="meteo.png")
 img_bg = tk.PhotoImage(file="cosmo.png")
 main()
